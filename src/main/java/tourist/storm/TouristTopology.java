3e14a20805d5986190a8d481d3896727948aa85e
yangzq <yzq6711@gmail.com>
2013-03-04 15:52:04 +0800

    拓扑定义中添加本地模式判断

--- src/main/java/tourist/storm/TouristTopology.java
+++ src/main/java/tourist/storm/TouristTopology.java
@@ -1,11 +1,13 @@
 package tourist.storm;
 
 import backtype.storm.Config;
+import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
+import backtype.storm.utils.Utils;
 import tourist.util.MetricsDetector;
 
 /**
@@ -18,11 +20,23 @@ public class TouristTopology {
         TopologyBuilder builder = getTopologyBuilder();
         Config conf = new Config();
         conf.setDebug(true);
-        conf.setNumWorkers(10);
-        conf.setMaxSpoutPending(100);
-        conf.setNumAckers(10);
-        conf.setMessageTimeoutSecs(5);
-        StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+
+        if (args!=null && args.length > 0) { // 远程模式
+            System.out.println("Remote mode");
+            conf.setNumWorkers(10);
+            conf.setMaxSpoutPending(100);
+            conf.setNumAckers(10);
+            conf.setMessageTimeoutSecs(5);
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        } else {
+            // 本地模式，调试代码
+            System.out.println("Local mode");
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("touristTopology", conf, builder.createTopology());
+
+            Utils.sleep(10000);
+            cluster.shutdown();
+        }
     }
 
     public static TopologyBuilder getTopologyBuilder() {
@@ -36,7 +50,7 @@ public class TouristTopology {
         builder.setBolt("touristCountChangeBolt", new TouristCountChangeBolt(
                 new MetricsDetector.Metrics(8 * ONE_HOUR, 18 * ONE_HOUR, 3 * ONE_HOUR, 5),
                 new MetricsDetector.Metrics(18 * ONE_HOUR, 8 * ONE_HOUR, 3 * ONE_HOUR, 5)
-        ), 4).fieldsGrouping(signalingSpout, SignalingSpout.SIGNALING, new Fields("imsi"))
+        ), 1).fieldsGrouping(signalingSpout, SignalingSpout.SIGNALING, new Fields("imsi"))
                 .allGrouping(updateTimeBolt, UpdateTimeBolt.UPDATE_TIME);
         builder.setBolt("touristCountBolt", new TouristCountBolt(), 1).globalGrouping("touristCountChangeBolt");
         return builder;
